4010ab04e24aa8a2956ec04f7d19a1417dc8aa2a
tarek.fouda <tarek.fouda@roaa.tech>
2023-12-23 10:25:21 +0200

    production link

--- lib/core/http/generic_http/api_names.dart
+++ lib/core/http/generic_http/api_names.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: constant_identifier_names
 
 class ApiNames {
-  static const String baseUrl = "https://staging.suliit.com/api/v3/";
+  ///Development
+  // static const String baseUrl = "https://staging.suliit.com/api/v3/";
+  ///Production
+  static const String baseUrl = "https://suliit.com/api/v3/";
 
-  // static const String baseUrl = "https://suliit.com/api/v3/";
-  // auth routes
+  /// auth routes
   static const String login = "login";
   static const String savePublicKey = "users/";
   static const String updateUser = "users/";
