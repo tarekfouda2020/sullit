fb20ea305fa5a0d44f133529cb4e53302a82820d
tarekfouda <tarek.fouda@roaa.tech>
2025-10-04 13:49:13 +0300

    update app name from Sullit to MCOOP in AndroidManifest, Info.plist, and my_app.dart

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -51,7 +51,7 @@ class _MyAppState extends State<MyApp> {
               builder: (theme, darkTheme) {
                 return MaterialApp.router(
                   debugShowCheckedModeBanner: false,
-                  title: "Sullit",
+                  title: "MCOOP",
                   darkTheme: darkTheme,
                   theme: theme,
                   supportedLocales: const [Locale('en', 'US'), Locale('ar', 'EG'),Locale('bn', 'BD')],
