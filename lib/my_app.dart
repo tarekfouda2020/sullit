8c571f93abc757d6fd923a56d6312c1fc400d7eb
tarekfouda <tarekfouda0@gmail.com>
2025-12-05 22:29:54 +0200

    change app name

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -51,7 +51,7 @@ class _MyAppState extends State<MyApp> {
               builder: (theme, darkTheme) {
                 return MaterialApp.router(
                   debugShowCheckedModeBanner: false,
-                  title: "MCOOP",
+                  title: "AMCOOP",
                   darkTheme: darkTheme,
                   theme: theme,
                   supportedLocales: const [Locale('en', 'US'), Locale('ar', 'EG'),Locale('bn', 'BD'), Locale('ur', 'PK')],
