f35f5a28fdcffaeefc37b1062a44507c90541d3f
tarekfouda <tarekfouda0@gmail.com>
2025-12-25 23:02:20 +0200

    add facebook events

---
+++ lib/core/helpers/facebook_events_helper.dart
@@ -0,0 +1,12 @@
+
+import 'package:facebook_app_events/facebook_app_events.dart';
+
+class FacebookEventsHelper {
+
+  FacebookEventsHelper._();
+
+  static FacebookEventsHelper  instance = FacebookEventsHelper._();
+
+   final FacebookAppEvents facebookAppEvents = FacebookAppEvents();
+
+}
