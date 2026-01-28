8222b12f02517d452ddf7089d7404d00e11bd1e7
tarekfouda <tarekfouda0@gmail.com>
2026-01-28 13:44:27 +0200

    edits

--- lib/features/general/auth/presentation/pages/login/widgets/switch_phone_email_widget.dart
+++ lib/features/general/auth/presentation/pages/login/widgets/switch_phone_email_widget.dart
@@ -8,39 +8,40 @@ class SwitchPhoneEmailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: 320,
-          child: Text(tr("loginWithEmailOrPhone"),
-          style: AppTextStyle.s13_w400(color: context.colors.textColor),
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(tr("loginWithEmailOrPhone"),
+          style: AppTextStyle.s13_w400(color: context.colors.textColor).copyWith(
+            height: 1.2
           ),
-        ),
-        Gaps.vGap16,
-        BlocBuilder<GenericBloc<int>, GenericState<int>>(
-          bloc: controller.switchEmailPhoneCubit,
-          builder: (context, state) {
-            return Row(
-             spacing: 24,
-              children: List.generate(controller.emailAndPhone.length, (index) {
-                return GestureDetector(
-                  onTap: () => controller.switchEmailAndPhone(index),
-                  child: Row(
-                    children: [
-                      CustomRadioWidget(selected: state.data==index),
-                      Gaps.hGap8,
-                      Text(controller.emailAndPhone[index],
-                        style: AppTextStyle.s14_w700(color: context.colors.black),
-                      )
-                    ],
-                  ),
-                );
-              }),
-            );
-          },
-        ),
-      ],
+          ),
+          Gaps.vGap16,
+          BlocBuilder<GenericBloc<int>, GenericState<int>>(
+            bloc: controller.switchEmailPhoneCubit,
+            builder: (context, state) {
+              return Row(
+               spacing: 24,
+                children: List.generate(controller.emailAndPhone.length, (index) {
+                  return GestureDetector(
+                    onTap: () => controller.switchEmailAndPhone(index),
+                    child: Row(
+                      children: [
+                        CustomRadioWidget(selected: state.data==index),
+                        Gaps.hGap8,
+                        Text(controller.emailAndPhone[index],
+                          style: AppTextStyle.s14_w700(color: context.colors.black),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
