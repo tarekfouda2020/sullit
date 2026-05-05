5ba73c6848577629004c90a86347caf34e55f914
tarekfouda <tarekfouda0@gmail.com>
2026-05-05 11:18:37 +0300

    update app version

--- lib/features/user/products/presentation/pages/seller_products/widgets/seller_details_app_bar.dart
+++ lib/features/user/products/presentation/pages/seller_products/widgets/seller_details_app_bar.dart
@@ -29,7 +29,7 @@ class SellerDetailsAppBar extends StatelessWidget {
                   left: 16,
                   right: 16),
               child: shopModel != null
-                  ? SellerCardWidget(shop: shopModel)
+                  ? SellerCardWidget(shop: shopModel, openImage: true)
                   : const BuildShimmerItem(height: 300),
             ),
           ),
