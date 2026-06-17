caab626f3bf89a8b49f642c7929498dc90c71410
tarekfouda <tarekfouda0@gmail.com>
2026-06-17 10:37:23 +0300

    implement expandable product description with plain-text formatting and update app version to 1.0.50

--- lib/features/user/products/presentation/pages/product_details/widgets/build_product_qty.dart
+++ lib/features/user/products/presentation/pages/product_details/widgets/build_product_qty.dart
@@ -9,84 +9,81 @@ class BuildProductQty extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Container(
-        padding: const EdgeInsetsDirectional.only(start: 23,end: 25,top:14 ,bottom:13),
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        decoration:  CustomDecoration(
-          radius: BorderRadius.circular(Dimens.dp12)
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Visibility(
-              // visible: (productModel.variant?.currentStock ?? 0) > 0,
-              child: Text(
-                "${tr("quantity")} : ",
-                style: AppTextStyle.s14_w400(
-                  color: context.colors.textColor,
-                ),
+    return Container(
+      padding: const EdgeInsetsDirectional.only(start: 23,end: 25,top:14 ,bottom:13),
+      decoration:  CustomDecoration(
+        radius: BorderRadius.circular(Dimens.dp12)
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Visibility(
+            // visible: (productModel.variant?.currentStock ?? 0) > 0,
+            child: Text(
+              "${tr("quantity")} : ",
+              style: AppTextStyle.s14_w400(
+                color: context.colors.textColor,
               ),
             ),
-            Visibility(
-              visible: productModel.isOutOfStock == false,
-              replacement: Text(tr("outOfStock"),
-                style: AppTextStyle.s15_w700(color: context.colors.primary),
-              ),
-              child: BlocBuilder<GenericBloc<int>, GenericState<int>>(
-                bloc: controller.qtyCubit,
-                builder: (context, state) {
-                  // bool hasVariant = productModel.variant != null;
-                  return Row(
-                    children: [
-                      // Visibility(
-                      //   visible: hasVariant ? productModel.variant!.currentStock! > 0 : false,
-                      //   replacement: Text(
-                      //     tr("outOfStock"),
-                      //     style: AppTextStyle.s15_w700(
-                      //       color: context.colors.primary,
-                      //     ),
-                      //   ),
-                      //   child: Text(
-                      //     "(${productModel.variant?.currentStock} ${tr("available")})",
-                      //     style: AppTextStyle.s14_w400(
-                      //       color: context.colors.black,
-                      //     ),
-                      //   ),
-                      // ),
-                      // Gaps.hGap10,
-                      // Text(
-                      //   "${productModel.variant?.calculablePrice} ${productModel.currencySymbol}",
-                      //   style: AppTextStyle.s18_w500(
-                      //     color: context.colors.primary,
-                      //   ),
-                      // ),
-                      // Gaps.hGap10,
-                      BuildCustomBounce(
-                        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 7.5),
-                        onTap: () => controller.decreaseQty(),
-                        iconData: CupertinoIcons.minus,
-                        margin: EdgeInsets.zero,
-                      ),
-                      Gaps.hGap19,
-                      Text(
-                        "${state.data}",
-                        style: AppTextStyle.s18_w600(color: context.colors.black),
-                      ),
-                      Gaps.hGap19,
-                      BuildCustomBounce(
-                        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 7.5),
-                        onTap: () => controller.increaseQty(),
-                        iconData: CupertinoIcons.add,
-                        margin: EdgeInsets.zero,
-                      ),
-                    ],
-                  );
-                },
-              ),
-            )
-          ],
-        ),
+          ),
+          Visibility(
+            visible: productModel.isOutOfStock == false,
+            replacement: Text(tr("outOfStock"),
+              style: AppTextStyle.s15_w700(color: context.colors.primary),
+            ),
+            child: BlocBuilder<GenericBloc<int>, GenericState<int>>(
+              bloc: controller.qtyCubit,
+              builder: (context, state) {
+                // bool hasVariant = productModel.variant != null;
+                return Row(
+                  children: [
+                    // Visibility(
+                    //   visible: hasVariant ? productModel.variant!.currentStock! > 0 : false,
+                    //   replacement: Text(
+                    //     tr("outOfStock"),
+                    //     style: AppTextStyle.s15_w700(
+                    //       color: context.colors.primary,
+                    //     ),
+                    //   ),
+                    //   child: Text(
+                    //     "(${productModel.variant?.currentStock} ${tr("available")})",
+                    //     style: AppTextStyle.s14_w400(
+                    //       color: context.colors.black,
+                    //     ),
+                    //   ),
+                    // ),
+                    // Gaps.hGap10,
+                    // Text(
+                    //   "${productModel.variant?.calculablePrice} ${productModel.currencySymbol}",
+                    //   style: AppTextStyle.s18_w500(
+                    //     color: context.colors.primary,
+                    //   ),
+                    // ),
+                    // Gaps.hGap10,
+                    BuildCustomBounce(
+                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 7.5),
+                      onTap: () => controller.decreaseQty(),
+                      iconData: CupertinoIcons.minus,
+                      margin: EdgeInsets.zero,
+                    ),
+                    Gaps.hGap19,
+                    Text(
+                      "${state.data}",
+                      style: AppTextStyle.s18_w600(color: context.colors.black),
+                    ),
+                    Gaps.hGap19,
+                    BuildCustomBounce(
+                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 7.5),
+                      onTap: () => controller.increaseQty(),
+                      iconData: CupertinoIcons.add,
+                      margin: EdgeInsets.zero,
+                    ),
+                  ],
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
     return SliverToBoxAdapter(
